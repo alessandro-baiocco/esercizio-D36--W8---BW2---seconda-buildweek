f5a7bc4eef80a148bedb330a06c96a6c2ea7e4f6
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-08-30 12:53:52 +0200

    u

--- desktop-1.js
+++ desktop-1.js
@@ -1,5 +1,36 @@
+const apiKey = 'X-RapidAPI-Key'
+const apiValue = 'c39ea51001msh0b48f18ff7528dfp178bb6jsnbcb227f7b0fe'
+
+window.onload = () => {
+    // const artistId = new URLSearchParams(window.location.search).get("artistId");
+    // const re = await fetch(serverUrl + "/" + productId, {
+    //     method: "GET",
+    //     headers: {
+    //     Authorization: apiValue,
+    //     },
+    // });
+    // if (!re.ok) {
+    //     throw new Error("error");
+    // }
+    // const product = await re.json();
+    const artistId = 412
+    const fetchUrl = `https://api.deezer.com/artist/${artistId}/top?limit=6`
+    const re = await fetch(fetchUrl, {
+        method: "GET",
+        headers: {
+        'X-RapidAPI-Key': apiValue,
+        },
+    });
+    if (!re.ok) {
+        throw new Error("error");
+    }
+    const product = await re.json();
+}
+
 const canzone = document.querySelectorAll(".canzone");
 const player = document.getElementById("player");
+
+const artistId = ''
 
 canzone.forEach(canzone => {
   canzone.addEventListener("click", () => {
