bcc776fc9b9d3468ed7c32d2fe490477ac485b44
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-08-31 09:24:48 +0200

    some cose

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,5 @@
 const url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=Queen";
+let fetchedContentId = [];
 
 fetch(url, {
   headers: {
@@ -12,9 +13,15 @@ fetch(url, {
     const leCardSU = document.querySelectorAll(".albumSu");
     const leCardCentro = document.querySelectorAll(".albumCentro");
     const leCardGiu = document.querySelectorAll(".albumGiu");
+    let num = 0;
+    let num2 = 0;
+    let num3 = 0;
+    let num4 = 0;
 
     for (let i = 0; i < leSeiCard.length; i++) {
-      leSeiCard[i].innerHTML = `<a href="album.html/${albumObj.data[i].album.id}"
+      if (!fetchedContentId.includes(albumObj.data[i].album.id)) {
+        fetchedContentId.push(albumObj.data[i].album.id);
+        leSeiCard[num].innerHTML = `<a href="album.html/${albumObj.data[i].album.id}"
         ><div class="card border border-0 p-0 consigliatiCard">
         <div class="row gap-0 m-0 d-flex align-items-center">
         <div class="col-3 p-0">
@@ -32,23 +39,27 @@ fetch(url, {
         </div>
         </div></a
         >`;
+        num++;
+      }
 
       for (let j = 0; j < leCardSU.length; j++) {
-        leCardSU[j].innerHTML = `
+        if (!fetchedContentId.includes(albumObj.data[j].artist.id)) {
+          fetchedContentId.push(albumObj.data[j].artist.id);
+          leCardSU[num2].innerHTML = `
                     </a><div class="d-flex flex-md-column">
                     <div>       
                     <a href = "album.html?idAlbum=${albumObj.data[j].artist.id}"><img src="${
-          albumObj.data[j].artist.picture_medium
-        }" alt="" class="img-fluid"  /></a>
+            albumObj.data[j].artist.picture_medium
+          }" alt="" class="img-fluid"  /></a>
                     </div>
                     <div class="card-body ps-2">
                     <p class="d-block d-md-none">Playlist</p>
                     <a href = "album.html?idAlbum=${albumObj.data[j].artist.id}"><h4 class = "fs-3">${
-          albumObj.data[j].artist.name
-        }</h4></a>
+            albumObj.data[j].artist.name
+          }</h4></a>
                     <a href = "album.html?idAlbum=${albumObj.data[j].artist.id}"><p>il meglio di ${
-          albumObj.data[j].artist.name
-        }</p></a>
+            albumObj.data[j].artist.name
+          }</p></a>
                     </div>
                     </div>
                     <div class="d-flex d-md-none">
@@ -61,23 +72,26 @@ fetch(url, {
                     <button class="heart btn"><i class="bi bi-play-circle text-light fs-3"></i></button>
                     </div>
                     </div>`;
+          num2++;
+        }
       }
       for (let n = 6; n < leCardCentro.length + 6; n++) {
-        leCardCentro[n - 6].innerHTML = `
+        if (!fetchedContentId.includes(albumObj.data[n].album.id)) {
+          leCardCentro[num3].innerHTML = `
             <div class="d-flex flex-md-column">
             <div>
             <a href = "album.html?idAlbum=${albumObj.data[n].album.id}"><img src="${
-          albumObj.data[n].album.cover_medium
-        }" alt="" class="img-fluid"  /></a>
+            albumObj.data[n].album.cover_medium
+          }" alt="" class="img-fluid"  /></a>
             </div>
               <div class="card-body  ps-2">
               <p class="d-block d-md-none">Playlist</p>
               <a href = "album.html?idAlbum=${albumObj.data[n].album.id}"><h4 class = "fs-3">${
-          albumObj.data[n].album.title
-        }</h4></a>
+            albumObj.data[n].album.title
+          }</h4></a>
               <a href = "album.html?idAlbum=${albumObj.data[n].album.id}"><p>il meglio di ${
-          albumObj.data[n].album.title
-        }</p></a>
+            albumObj.data[n].album.title
+          }</p></a>
               </div>
               </div>
               <div class="d-flex d-md-none">
@@ -90,36 +104,8 @@ fetch(url, {
               <button class="heart btn"><i class="bi bi-play-circle text-light fs-3"></i></button>
               </div>
               </div>`;
-      }
-
-      for (let m = 8; m < leCardGiu.length + 8; m++) {
-        leCardGiu[m - 8].innerHTML = `
-            <div class="d-flex flex-md-column">
-            <div>
-            <a href = "desktop-1.html?idArtist=${albumObj.data[m].album.id}"><img src="${
-          albumObj.data[m].album.cover_medium
-        }" alt="" class="img-fluid"  /></a>
-            </div>
-              <div class="card-body ps-2">
-              <p class="d-block d-md-none">Playlist</p>
-              <a href = "desktop-1.html?idArtist=${albumObj.data[m].album.id}"><h4 class = "fs-3">${
-          albumObj.data[m].album.title
-        }</h4></a>
-              <a href = "desktop-1.html?idArtist=${albumObj.data[m].album.id}"><p>il meglio di ${
-          albumObj.data[m].album.title
-        }</p></a>
-              </div>
-              </div>
-              <div class="d-flex d-md-none">
-              <div class="me-auto">
-              <button class="heart btn"><i class="bi bi-heart text-light fs-3"></i></button>
-              <button class="heart btn"><i class="bi bi-three-dots-vertical text-light fs-3"></i></button>
-              </div>
-              <div class="d-flex align-items-center">
-              <p class="mb-0">${Math.floor(Math.random() * 30 + 10)} brani</p>
-              <button class="heart btn"><i class="bi bi-play-circle text-light fs-3"></i></button>
-              </div>
-              </div>`;
+          num3++;
+        }
       }
     }
   });
